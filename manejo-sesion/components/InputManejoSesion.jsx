843766f1b8b2a17414cd04b8f56d94398c8fbcc3
Aluon <paulacanoroman@gmail.com>
2023-09-13 10:10:02 +0200

    funciones recuperar y salir de sesion

--- manejo-sesion/components/InputManejoSesion.jsx
+++ manejo-sesion/components/InputManejoSesion.jsx
@@ -6,14 +6,27 @@ const InputManejoSesion = () => {
 
     const handleSaveData = ()=>{
         localStorage.setItem("datos sesion", JSON.stringify(userData))
+        setUserData("")
     }
+
+    const handleRecoverData = ()=>{
+        const localData = localStorage.getItem("datos sesion")
+        setUserData(JSON.parse(localData))
+    }
+
+    const handleExit = ()=>{
+        localStorage.removeItem("datos sesion")
+    }
+
 
 
   return (
     <>
-    <input type='text' onChange={(e)=>{setUserData({...userData,username:e.target.value})}}></input>
+    <input type='text' onChange={(e)=>{setUserData({...userData,username:e.target.value})}} value={userData.username ?? ""}></input>
 
     <button onClick={()=>{handleSaveData()}}>Iniciar sesión</button>
+    <button onClick={()=>{handleRecoverData()}}>Recuperar sesión</button>
+    <button onClick={()=>{handleExit()}}>Salir de sesión</button>
     </>
   )
 }
